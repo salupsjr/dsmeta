2bf2733aa27f1ed3e41c165c7c99030025d1a33d
Salustiano Junior <salups@hotmail.com>
2022-11-20 18:19:43 -0300

    Axios, useEffect first request - Inclusao do Axios e useEffect first request

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,8 +9,7 @@ function Header() {
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
                 <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                    Desenvolvido por Salustiano Junior
                 </p>
             </div>
         </header>
